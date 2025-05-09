6c7cb247c10e276bb02ab7519d1a56c49a96498a
miniharu22 <isangmin219@gmail.com>
2025-05-09 15:26:30 +0900

    code copy

--- assets/js/clipboardrouge.js
+++ assets/js/clipboardrouge.js
@@ -1,70 +1,48 @@
-// Clipboard
-// This makes the button blink 250 miliseconds
-
+/* Simple copy to clipboard action
+ * ---
+ * This script adds a simple "copy to clipboard" button to code elements located within <pre> elements. 
+ * Requires jQuery. Can be used with the Jekyll Minimal Mistakes theme by adding it to the 'after_footer_scripts'.
+ */
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function buttonBlink(btn, style) {
-  btn.classList.remove("btn-light");
-  btn.classList.add(style);
-  await sleep(250); //Blink ms
-  btn.classList.remove(style);
-  btn.classList.add("btn-light");
+async function onClickEffect(btn, style) {
+  btn.removeClass("btn-light");
+  btn.addClass(style);
+  await sleep(250);
+  btn.removeClass(style);
+  btn.addClass("btn-light");
 }
-// End
 
+$(document).ready(function() {
+  // Create butons
+  $(".page__content pre > code").each(function() {
+      $(this).parent().prepend(
+          $(document.createElement('button')).prop({
+              type: 'button',
+              innerHTML: 'copy',
+          })
+          .attr('title', 'Copy to clipboard')
+          .addClass('btn')
+          .addClass('btn--primary')
+          .css('position', 'absolute')
+          .css('right', '1em')
+          // Click listener
+          .on('click', function() {
+              let codeElement = $(this).parent().children('code').first();
 
-// Select highlghted codes
-var codeChunk = document.querySelectorAll("pre.highlight");
+              if (!codeElement) {
+                  throw new Error("Unexpected error! No corresponding code block was found for this button.");
+              }
 
-// Loop to add buttons
-for (var i = 0; i < codeChunk.length; i++) {
+              // Blink effect
+              onClickEffect($(this), "btn--success")
 
-  var pre = codeChunk.item(i);
-  var btn = document.createElement("button");
-  // Prepare button
-  btn.innerHTML = "<i class='fa-solid fa-copy'></i>"; // Icon to be displayed on the button
-
-  // Inline styling - may be a new css class, to be added in the next section
-  btn.style.position = "absolute";
-  btn.style.right = "1em";
-
-  // Button: CSS - Add new classes
-  btn.classList.add("btn", "btn--primary");
-
-  // Identifier for ClipboardJS
-  btn.setAttribute("data-clipboard-copy", "");
-
-  // aria-label: btn.setAttribute("aria-label", "Copy to clipboard");
-  // etc.
-
-  // Insert button
-  pre.insertBefore(btn, pre.firstChild);
-
-}
-// End
-
-// Copy to clipboard
-var clipboard = new ClipboardJS("[data-clipboard-copy]", {
-  target: function (trigger) {
-    return trigger.nextElementSibling;
-  }
+              // Copy to clipoard function
+              navigator.clipboard.writeText($(codeElement).text()).then(() => true, () => true);
+              return true;
+          })
+      );
+  });
 });
-
-// Messages and make the button blink
-clipboard.on("success", function (e) {
-  e.clearSelection();
-  buttonBlink(e.trigger, "btn--success");
-  console.info("Action:", e.action);
-  console.info("Text:", e.text);
-  console.info("Trigger:", e.trigger);
-});
-
-clipboard.on("error", function (e) {
-  e.clearSelection();
-  buttonBlink(e.trigger, "btn--danger");
-  console.info("Action:", e.action);
-  console.info("Trigger:", e.trigger);
-});
-// Finish
